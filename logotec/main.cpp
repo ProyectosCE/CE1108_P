07c42e4473363b25709c179348b53fdd9f772808
Alexander Montero V <alexmv223@pm.me>
2025-09-27 20:02:05 -0600

    [ADD] start antlr4

--- logotec/main.cpp
+++ logotec/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include "antlr4-runtime.h"
+
+using namespace antlr4;
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+
+    ifstream stream;
+    stream.open("input.txt");
+    ANTLRInputStream input(stream);
+
     return 0;
 }
