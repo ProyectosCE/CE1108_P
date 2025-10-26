93539d8d07094a9ca9e24e4f2e905d150fdf1e65
joscCE <josc4188@gmail.com>
2025-10-26 00:00:13 -0600

    base

--- logotec/out_files/turtle_hw.cpp
+++ logotec/out_files/turtle_hw.cpp
@@ -1,40 +1,148 @@
 #include "turtle_hw.h"
-
+#include <Servo.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
 // Pines
-#define R_wheel 0  // Rueda izquierda
-#define L_wheel 2  // Rueda derecha
-#define rotate 16  // Girar
-#define color_change 5  // Cambio color
-#define pen 4  // subelapiz
+#define IN1 0  // Rueda izquierda
+#define IN2 2
+
+#define IN3 12  // Rueda derecha
+#define IN4 15
+
+#define rotate 13  // Girar
+
+Servo Color_turrent;
+
+Adafruit_MPU6050 mpu;
+
+float anguloZ = 0.0;
+float ultimoZ = 0.0;
+unsigned long ultimoTiempo = 0;
+
+
+
 
 void initTurtle() {
-    pinMode(R_wheel, OUTPUT);
-    pinMode(L_wheel, OUTPUT);
+    pinMode(IN1, OUTPUT);
+    pinMode(IN3, OUTPUT);
+
+    pinMode(IN2, OUTPUT);
+    pinMode(IN4, OUTPUT);
+
     pinMode(rotate, OUTPUT);
-    pinMode(color_change, OUTPUT);
-    pinMode(pen, OUTPUT);
+
+
+    Color_turrent.attach(rotate);
+    Serial.begin(115200);
+    Wire.begin(4, 5); // SDA, SCL
+
+    if (!mpu.begin()) {
+        Serial.println("No se detectó MPU6050!");
+        while (1) delay(10);
+    }
+
+    mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+    mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+    mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+    delay(1000);
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+    ultimoZ = g.gyro.z;
+    ultimoTiempo = millis();
+
+    Serial.println("MPU6050 listo y calibrado.");
+
+
+
+
+}
+
+void detenerMotores() {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    esperar(2000);
 }
 
 void esperar(int n){
     delay(n);
 }
 
-void avanzaTortuga(double n) {
-    digitalWrite(R_wheel, HIGH);
-    digitalWrite(L_wheel, HIGH);
+float leerAnguloZ() {
+    sensors_event_t a, g, temp;
+    mpu.getEvent(&a, &g, &temp);
+
+    unsigned long tiempoActual = millis();
+    float dt = (tiempoActual - ultimoTiempo) / 1000.0; // segundos
+    ultimoTiempo = tiempoActual;
+
+    float velocidadZ = g.gyro.z * 57.2958; // rad/s → deg/s
+    anguloZ += velocidadZ * dt;
+    return anguloZ;
 }
+
+void resetAngulo() {
+    anguloZ = 0.0;
+    ultimoTiempo = millis();
+}
+
+
+
+
+
+void avanzaTortuga(double n) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    esperar(n*1000);
+    detenerMotores();
+
+
+
+}
+
+
+
 
 void retrocedeTortuga(double n) {
-    digitalWrite(R_wheel, HIGH);
-    digitalWrite(L_wheel, HIGH);
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    esperar(n*1000);
+    detenerMotores();
+
+
 }
 
-void giraDerecha(double n) {
-    digitalWrite(rotate, HIGH);
+void giraDerecha(double grados) {
+    resetAngulo();
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+
+    while (fabs(leerAnguloZ()) < grados) {
+        delay(10);
+    }
+    detenerMotores();
 }
 
-void giraIzquierda(double n) {
-    digitalWrite(rotate, HIGH);
+void giraIzquierda(double grados) {
+    resetAngulo();
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+
+    while (fabs(leerAnguloZ()) < grados) {
+        delay(10);
+    }
+    detenerMotores();
 }
 
 void ocultaTortuga() {
@@ -58,18 +166,20 @@ void ponY(double y) {
 }
 
 void bajaLapiz() {
-    digitalWrite(pen, HIGH);
+
 }
 
 void subeLapiz() {
-    digitalWrite(pen, LOW);
+
 }
 
 void ponColorLapiz(const char* color) {
-    digitalWrite(color_change, HIGH);
-    esperar(2000);
-    digitalWrite(color_change, LOW);
-    esperar(2000);
+        Color_turrent.write(90);
+        esperar(1000);
+        Color_turrent.write(0);
+        esperar(1000);
+
+
 }
 
 void centro() {
@@ -97,6 +207,9 @@ int POTENCIA(int a, int b) {
     }
     return result;
 }
+
+
+
 
 int AZAR(int n) {
     return rand() % (n + 1);
